5657e071d7d62d5cd7b73feb9a9a8264b411e0b9
rachwongrw <rwrachelwong@gmail.com>
2021-01-16 13:55:31 -0800

    add model

---
+++ TeamPage/Models/Member.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TeamPage.Models
+{
+    public class Member
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Bio { get; set; }
+        public string Picture { get; set; }
+
+        public Member(string name, string title, string bio, string picture)
+        {
+            Name = name;
+            Title = title;
+            Bio = bio;
+            Picture = picture;
+        }
+    }
+}
