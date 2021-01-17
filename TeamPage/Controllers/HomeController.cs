8630a92e69bd93a6e516aeca0176b4f97e6d1cbb
rachwongrw <rwrachelwong@gmail.com>
2021-01-17 15:45:23 -0800

    edit to styling

--- TeamPage/Controllers/HomeController.cs
+++ TeamPage/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Text.Encodings.Web;
 using TeamPage.Models;
 
 namespace TeamPage.Controllers
@@ -14,21 +15,20 @@ namespace TeamPage.Controllers
         private readonly ILogger<HomeController> _logger;
         public List<Member> Members { get; set; } = new List<Member>();
 
-        public void onGet()
-        {
-            Members = new List<Member>()
-            {
-                new Member("Mark Pontiac", "Manager", "He loves skiing on the weekend", "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80")
-            };
-        }
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        
         public IActionResult Index()
         {
+            Members = new List<Member>()
+            {
+                new Member("Mark Pontiac", "Manager", "He loves skiing on the weekend", "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80"),
+                new Member("ANOTHER person", "Fun Coordinator", "She's a basic bitch", "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80")
+            };
+            Console.WriteLine("Members", Members);
             return View();
         }
 
