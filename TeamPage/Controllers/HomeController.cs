0d5b1c191a91012f651565ad5e0ab3baaf6be536
rachwongrw <rwrachelwong@gmail.com>
2021-01-16 17:59:50 -0800

    working on adding foreach loop for team members

--- TeamPage/Controllers/HomeController.cs
+++ TeamPage/Controllers/HomeController.cs
@@ -12,6 +12,15 @@ namespace TeamPage.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public List<Member> Members { get; set; } = new List<Member>();
+
+        public void onGet()
+        {
+            Members = new List<Member>()
+            {
+                new Member("Mark Pontiac", "Manager", "He loves skiing on the weekend", "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80")
+            };
+        }
 
         public HomeController(ILogger<HomeController> logger)
         {
--- TeamPage/Models/Member.cs
+++ TeamPage/Models/Member.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
 namespace TeamPage.Models
 {
     public class Member
